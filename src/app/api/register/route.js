631a4bd083e7b2210ce64020e5d5645e1f77f03d
Yutthaphichai <yutthaphichaikk@gmail.com>
2024-12-10 11:14:43 +0700

    connect db mongodb

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,12 +1,15 @@
 import { NextResponse } from "next/server";
+import { connectMongoDB } from "../../../../lib/mongodb";
+import User from "../../../../models/user";
+import bcrypt from "bcryptjs";
 
 export async function POST(req) {
   try {
-    const { name, email, password } = await req.json;
+    const { name, email, password } = await req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log("name", name);
-    console.log("email", email);
-    console.log("password", password);
+    await connectMongoDB();
+    await User.create({ name, email, password: hashedPassword });
 
     return NextResponse.json({ message: "User registered." }, { status: 201 });
   } catch (error) {
